ab264783213abe4a3fd085325b185b33ee565170
Georgiy Veshchev <ghora2008@gmail.com>
2026-04-20 17:24:44 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,7 @@
 'use strict';
 
+// const { createElement } = require('react');
+
 const food = {
   Drink: {
     Wine: {},
@@ -21,7 +23,22 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  if (Object.keys(data).length === 0) {
+    return;
+  }
+
+  const newUl = document.createElement('ul');
+
+  for (const key in data) {
+    const newLi = document.createElement('li');
+
+    newLi.textContent = key;
+
+    createTree(newLi, data[key]);
+
+    newUl.append(newLi);
+  }
+  element.append(newUl);
 }
 
 createTree(tree, food);
