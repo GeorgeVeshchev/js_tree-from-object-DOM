290e055e3a12a511308edc12aa5e5ec305525b6a
Georgiy Veshchev <ghora2008@gmail.com>
2026-04-20 19:15:46 +0300

    solution 2.0

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -23,22 +23,31 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  if (Object.keys(data).length === 0) {
+  if (!element) {
+    return;
+  }
+
+  if (!data || Object.keys(data).length === 0) {
     return;
   }
 
   const newUl = document.createElement('ul');
 
   for (const key in data) {
-    const newLi = document.createElement('li');
+    if (Object.prototype.hasOwnProperty.call(data, key)) {
+      const newLi = document.createElement('li');
 
-    newLi.textContent = key;
+      newLi.textContent = key;
 
-    createTree(newLi, data[key]);
+      createTree(newLi, data[key]);
 
-    newUl.append(newLi);
+      newUl.append(newLi);
+    }
   }
-  element.append(newUl);
+
+  if (newUl.children.length > 0) {
+    element.append(newUl);
+  }
 }
 
 createTree(tree, food);
